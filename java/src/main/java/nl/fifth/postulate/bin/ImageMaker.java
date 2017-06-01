540e9de871a8c697481f0d2bf8e2409357c2d82c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-06-01 06:40:05 +0200

    Read program arguments

--- java/src/main/java/nl/fifth/postulate/bin/ImageMaker.java
+++ java/src/main/java/nl/fifth/postulate/bin/ImageMaker.java
@@ -10,14 +10,23 @@ import java.io.IOException;
 
 import static java.lang.Math.floor;
 
-public class FirstImage {
+public class ImageMaker {
     public static void main(String[] args) throws FitsException, IOException {
-        Fits f = new Fits("../k2-trappist1-unofficial-tpf-long-cadence.fits");
+        String filename = args[0];
+        String pathname = args[1];
+        int row = Integer.valueOf(args[2]);
+
+        createImage(filename, pathname, row);
+
+    }
+
+    private static void createImage(String filename, String pathname, int row) throws FitsException, IOException {
+        Fits f = new Fits(filename);
         BinaryTableHDU hdu = (BinaryTableHDU) f.getHDU(1);
         hdu.info(System.out);
         float[][][] rows = (float[][][]) hdu.getColumn("FLUX");
 
-        float[][] inputImage = rows[0];
+        float[][] inputImage = rows[row];
         BufferedImage outputImage = new BufferedImage(inputImage[0].length, inputImage.length, BufferedImage.TYPE_BYTE_GRAY);
         WritableRaster raster = outputImage.getRaster();
 
@@ -41,8 +50,7 @@ public class FirstImage {
             }
         }
 
-        File output = new File("src/main/resources/first-image.png");
+        File output = new File(pathname);
         ImageIO.write(outputImage, "png", output);
-
     }
 }
