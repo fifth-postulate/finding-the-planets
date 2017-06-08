63d2d76eac263c6e6bcb043061873d27f08fcd64
Daan van Berkel <Daan.vanBerkel@han.nl>
2017-06-08 14:04:52 +0200

    List the correct time on the average.csv

--- java/src/main/java/nl/fifth/postulate/bin/Average.java
+++ java/src/main/java/nl/fifth/postulate/bin/Average.java
@@ -14,33 +14,50 @@ public class Average {
         PrintStream output = new PrintStream(new FileOutputStream(pathName));
 
 
-        float[] average = calculateAverage(filename);
+        AverageDataPair[] average = calculateAverage(filename);
 
         for (int index = 0, limit = average.length; index < limit; index++) {
-            output.printf("%4d, %f\n", index, average[index]);
+            output.printf("%4d, %s\n", index, average[index]);
         }
 
     }
 
-    private static float[] calculateAverage(String filename) throws FitsException, IOException {
+    private static AverageDataPair[] calculateAverage(String filename) throws FitsException, IOException {
         Fits f = new Fits(filename);
         BinaryTableHDU hdu = (BinaryTableHDU) f.getHDU(1);
-        float[][][] rows = (float[][][]) hdu.getColumn("FLUX");
+        double[] timeRows = (double[]) hdu.getColumn("TIME");
+        float[][][] fluxRows = (float[][][]) hdu.getColumn("FLUX");
 
-        float[] average = new float[rows.length];
-        for (int row = 0, rowLimit = rows.length; row < rowLimit; row++ ) {
+        AverageDataPair[] averageDataPair = new AverageDataPair[fluxRows.length];
+        for (int row = 0, rowLimit = fluxRows.length; row < rowLimit; row++ ) {
             float sum = 0.0f;
             int count = 0;
-            float[][] inputImage = rows[row];
+            float[][] inputImage = fluxRows[row];
             for (int y = 0, ylimit = inputImage.length; y < ylimit; y++) {
                 for (int x = 0, xlimit = inputImage[0].length; x < xlimit; x++) {
                     sum += inputImage[y][x];
                     count += 1;
                 }
             }
-            average[row] = sum / count;
+            float average = sum / count;
+
+            averageDataPair[row] = new AverageDataPair(timeRows[row], average);
         }
 
-        return average;
+        return averageDataPair;
+    }
+}
+
+class AverageDataPair {
+    private final double time;
+    private final float average;
+
+    public AverageDataPair(double time, float average) {
+        this.time = time;
+        this.average = average;
+    }
+
+    public String toString() {
+        return String.format("%10.6f, %10.6f", time, average);
     }
 }
