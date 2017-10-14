9f4177f16f0baa918f991a2746881442f1530a08
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-14 07:08:23 +0200

    Create a groups function

--- rust-showcase/src/mad/mod.rs
+++ rust-showcase/src/mad/mod.rs
@@ -15,6 +15,20 @@ pub fn median_of(slice: &[f64]) -> f64 {
     result.clone()
 }
 
+pub fn groups(slice: Vec<f64>, group_size: usize) -> Vec<Vec<f64>> {
+    let mut groups: Vec<Vec<f64>> = vec!();
+
+    for end_index in group_size .. slice.len() + 1 {
+        let mut group: Vec<f64> = vec!();
+        for index in (end_index - group_size) .. end_index {
+            group.push(slice[index])
+        }
+        groups.push(group)
+    }
+
+    groups
+}
+
 #[cfg(test)]
 pub mod tests {
     const EPSILON: f64 = 0.00001;
@@ -37,5 +51,32 @@ pub mod tests {
 
         assert_eq!(median, 2.5);
         assert!((median - 2.5).abs() < EPSILON);
+    }
+
+    #[test]
+    fn groups_of_2_for_a_vector() {
+        let data: Vec<f64> = vec!(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+
+        let gs = groups(data, 2);
+
+        assert_eq!(vec![
+            vec!(1.0, 2.0),
+            vec!(2.0, 3.0),
+            vec!(3.0, 4.0),
+            vec!(4.0, 5.0),
+            vec!(5.0, 6.0)], gs);
+    }
+
+    #[test]
+    fn groups_of_3_for_a_vector() {
+        let data: Vec<f64> = vec!(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+
+        let gs = groups(data, 3);
+
+        assert_eq!(vec![
+            vec!(1.0, 2.0, 3.0),
+            vec!(2.0, 3.0, 4.0),
+            vec!(3.0, 4.0, 5.0),
+            vec!(4.0, 5.0, 6.0)], gs);
     }
 }
