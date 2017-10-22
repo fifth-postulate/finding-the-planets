1730b88d324f170850a380e09336266faf50dac4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-22 14:56:50 +0200

    Create a FloatIterator
    It iterates over all floating point numbers between start and finish seperated
    by a step

--- rust-showcase/src/fit/mod.rs
+++ rust-showcase/src/fit/mod.rs
@@ -1,1 +1,57 @@
-// Our fitting code
+use std::iter::Iterator;
+
+pub struct FloatIterator {
+    start: f64,
+    finish: f64,
+    step: f64,
+    current: u64,
+}
+
+impl FloatIterator {
+    pub fn new(start: f64, finish: f64, step: f64) -> Self {
+        Self { current: 0, start, finish, step }
+    }
+}
+
+impl Iterator for FloatIterator {
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let value = self.start + self.step * (self.current as f64);
+
+        if value <= self.finish {
+            self.current += 1;
+
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EPSILON: f64 = 0.000001;
+
+    #[test]
+    fn float_iterator_should_collect_floating_point_numbers() {
+        let mut iter = FloatIterator::new(0.0, 1.0, 0.2);
+
+        assert!(are_near(iter.next().unwrap(), 0.0f64));
+        assert!(are_near(iter.next().unwrap(), 0.2f64));
+        assert!(are_near(iter.next().unwrap(), 0.4f64));
+        assert!(are_near(iter.next().unwrap(), 0.6f64));
+        assert!(are_near(iter.next().unwrap(), 0.8f64));
+        assert!(are_near(iter.next().unwrap(), 1.0f64));
+        assert_eq!(iter.next(), None);
+    }
+
+    fn are_near(a: f64, b: f64) -> bool {
+        (a - b).abs() <= EPSILON
+    }
+}
+
+
