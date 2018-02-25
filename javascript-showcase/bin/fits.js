f7ab6e9887598781e75783c0775546578ddb1015
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-02-25 07:28:13 +0100

    List extension header

--- javascript-showcase/bin/fits.js
+++ javascript-showcase/bin/fits.js
@@ -1,6 +1,11 @@
 fits = require('fits');
 
+// fits.readFile('../k2-trappist1-unofficial-tpf-long-cadence.fits', function(error, FITS){
+//     if (error) return console.error(error);
+//     console.log(FITS.HDU.primary);
+// });
+
 fits.readFile('../k2-trappist1-unofficial-tpf-long-cadence.fits', function(error, FITS){
     if (error) return console.error(error);
-    console.log(FITS.HDU.primary);
+    console.log(FITS.HDU.extensions[0]);
 });
