f78bc9904e3cc0244be19cfae89d10df008a00e8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-02-26 10:58:26 +0100

    Show image

--- javascript-showcase/bin/fits.js
+++ javascript-showcase/bin/fits.js
@@ -5,7 +5,12 @@ fits = require('fits');
 //     console.log(FITS.HDU.primary);
 // });
 
+// fits.readFile('../k2-trappist1-unofficial-tpf-long-cadence.fits', function(error, FITS){
+//     if (error) return console.error(error);
+//     console.log(FITS.HDU.extensions[0]);
+// });
+
 fits.readFile('../k2-trappist1-unofficial-tpf-long-cadence.fits', function(error, FITS){
     if (error) return console.error(error);
-    console.log(FITS.HDU.extensions[0]);
+    console.log(FITS.images[1]);
 });
