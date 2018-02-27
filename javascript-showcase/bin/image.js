49f2ab7e5579ea33d2818c1729094c8b20a7b29f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-02-27 19:25:23 +0100

    Make use of defaults

--- javascript-showcase/bin/image.js
+++ javascript-showcase/bin/image.js
@@ -3,7 +3,8 @@ const parse = require('csv-parse');
 const stringify = require('csv-stringify');
 
 var input = fs.createReadStream('../workshop/long-cadence.csv');
-var parser = parse({ delimiter: ',' });
-var stringifier = stringify({ delimiter: ',' });
+var parser = parse();
+var stringifier = stringify();
+
 
 input.pipe(parser).pipe(stringifier).pipe(process.stdout);
