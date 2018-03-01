44d4e26fce7a6e2231c854a6635af533d3e780e4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-03-01 09:06:27 +0100

    Output row of data

--- javascript-showcase/bin/image.js
+++ javascript-showcase/bin/image.js
@@ -4,14 +4,15 @@ const stringify = require('csv-stringify');
 const transform = require('stream-transform');
 
 var input = fs.createReadStream('../workshop/long-cadence.csv');
+var output = fs.createWriteStream('out.csv');
 var parser = parse();
 var stringifier = stringify();
 var transformer = transform(function(data){
-    return data[0];
+    return [data[0]];
 });
 
 input
     .pipe(parser)
     .pipe(transformer)
     .pipe(stringifier)
-    .pipe(process.stdout);
+    .pipe(output);
