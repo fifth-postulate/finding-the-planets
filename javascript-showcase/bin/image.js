d5feedf022a171bb558e8d9e241d9f67f2e32999
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-03-01 08:45:15 +0100

    Create a transformer

--- javascript-showcase/bin/image.js
+++ javascript-showcase/bin/image.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 const parse = require('csv-parse');
 const stringify = require('csv-stringify');
+const transform = require('stream-transform');
 
 var input = fs.createReadStream('../workshop/long-cadence.csv');
 var parser = parse();
 var stringifier = stringify();
+var transformer = transform(function(data){
+    return data[0];
+});
 
-input.pipe(parser).pipe(stringifier).pipe(process.stdout);
+input
+    .pipe(parser)
+    .pipe(transformer)
+    .pipe(stringifier)
+    .pipe(process.stdout);
